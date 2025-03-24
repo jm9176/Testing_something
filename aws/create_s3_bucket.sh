28654534465e16f6609fc3ca67c9bb0a574fe545
Shubhdildeep Singh Sohal <shubh94@vt.edu>
2025-03-24 16:26:09 -0400

    Added s3 mock service

---
+++ aws/create_s3_bucket.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Create s3 bucket named "sample-bucket"
+awslocal s3 mb s3://sample-bucket
+
+# Add files to the s3 bucket and make them publicly readable
+awslocal s3 cp /tmp/localstack/data/sample_file.nc s3://sample-bucket/sample_file.nc --acl public-read
