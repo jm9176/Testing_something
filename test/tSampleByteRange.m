5b6b61302cb313ad6d47662d4caf24d963e0a892
Shubhdildeep Singh Sohal <shubh94@vt.edu>
2025-03-26 13:03:18 -0400

    Check byterange workflow

--- test/tSampleByteRange.m
+++ test/tSampleByteRange.m
@@ -1,18 +1,19 @@
 classdef tSampleByteRange < matlab.unittest.TestCase
-
+    % Sample test
+    
     properties(Constant)
         FilePath = 'http://localhost:4566/sample_file.nc#mode=bytes'
     end
 
     methods(Test)
-        function verifyFileInfo(testcase)
-            % Verify NetCDF file info using ncinfo function
-            actInfo = ncinfo(testcase.FilePath);
+        function verifyLocalFileInfo(testcase)
+            fileInp = fullfile('testfiles','sample_file.nc');
+            outInfo = ncinfo(fileInp);
         end
 
-        function verifyFileDisplay(testcase)
-            % Verify that a correct output is generated when using ncdisp with NetCDF file.
-            actDisp = evalc(['ncdisp(''' testcase.FilePath ''')']);
+        function verifyRemoteFileInfo(testcase)
+            disp(testcase.FilePath)
+            actInfo = ncinfo(testcase.FilePath);
         end
     end
 end
