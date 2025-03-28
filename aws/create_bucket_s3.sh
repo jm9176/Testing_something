1095dd9d5765a34bf77394eeb5da1b721fec07b7
Shubhdildeep Singh Sohal <shubh94@vt.edu>
2025-03-28 14:26:42 -0400

    Check code-coverage setup

--- aws/create_bucket_s3.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-aws s3api create-bucket --bucket my-test-bucket --endpoint-url=http://localhost:4566
-aws s3 cp /data/sample_file.nc s3://my-test-bucket/sample_file.nc --endpoint-url=http://localhost:4566
-aws s3 ls s3://my-test-bucket --endpoint-url=http://localhost:4566
