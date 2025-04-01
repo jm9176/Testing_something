4abd01ebca8485ff2f562da7c5e29e238bbfb4b3
Shubhdildeep Singh Sohal <shubh94@vt.edu>
2025-04-01 11:11:58 -0400

    Usign Localstack and boto3

--- create_bucket.py
+++ create_bucket.py
@@ -9,6 +9,7 @@ def main():
     # Initialize the S3 client
     s3 = boto3.client(
         's3',
+        endpoint_url=os.getenv('S3_ENDPOINT'),
         aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),
         aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY'),
         region_name=os.getenv('AWS_DEFAULT_REGION')
