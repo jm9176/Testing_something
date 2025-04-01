32ccb0157b8210558cd0e29e037a9b8409b83ecf
Shubhdildeep Singh Sohal <shubh94@vt.edu>
2025-04-01 10:20:21 -0400

    Using moto workflow

--- create_bucket.py
+++ create_bucket.py
@@ -1,26 +1,30 @@
 import boto3
-from moto import mock_aws
+import os
 
-@mock_aws
 def main():
     bucket_name = 'my-test-bucket'
     file_path = 'test/testfiles/sample_file.nc'
     file_name = 'sample_file.nc'
 
-    # Call the function to test
-    s3 = boto3.client('s3', region_name='us-east-1')
+    # Initialize the S3 client
+    s3 = boto3.client(
+        's3',
+        aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),
+        aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY'),
+        region_name=os.getenv('AWS_DEFAULT_REGION')
+    )
+
+    # Create a bucket
     s3.create_bucket(Bucket=bucket_name)
-    with open(file_path,"rb") as file_data:
-        s3.upload_fileobj(file_data,bucket_name,file_name)
-    
-    # Verify the bucket was created
+
+    # List buckets to verify creation
     response = s3.list_buckets()
     print('Buckets:', [bucket['Name'] for bucket in response['Buckets']])
-    
-    buckets = [bucket['Name'] for bucket in response['Buckets']]
-    assert bucket_name in buckets
 
-    # Verify the file was uploaded
+    # Upload a file to the bucket
+    s3.upload_file(file_path, bucket_name, file_name)
+
+    # List objects in the bucket
     response = s3.list_objects_v2(Bucket=bucket_name)
     print('Objects in bucket:', [obj['Key'] for obj in response.get('Contents', [])])
 
